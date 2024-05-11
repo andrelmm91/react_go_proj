7e4166b0cb635ecc85ef7e3ebaa61a6eb256ee43
andrelmm91 <andre.lmm91@gmail.com>
2024-05-11 22:12:00 +0200

    postgres db and conn

--- BE/cmd/api/main.go
+++ BE/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +11,9 @@ import (
 const port = 8080
 
 type application struct {
+	DSN    string
 	domain string
+	DB     *sql.DB
 }
 
 func main() {
@@ -17,13 +21,19 @@ func main() {
 	var app application
 
 	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres Connection String")
+	flag.Parse()
 
 	// connect to DB
-	app.domain = "example.com"
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
 
 	log.Println("Starting application on port", port)
 	// start web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatalf("error listening and serving to http: %s", err)
 	}
--- BE/godependencies.m
+++ BE/godependencies.m
@@ -2,3 +2,8 @@
 go get github.com/go-chi/chi/v5
 go get github.com/go-chi/chi/v5/middleware
 go get github.com/go-chi/cors
+
+//sql postgrtes
+go get github.com/jackc/pgx/v5
+go get github.com/jackc/pgconn
+go get github.com/jackc/pgx/v5/stdlib
