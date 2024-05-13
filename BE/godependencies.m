4a416ae23f492a6d7a36b161c7adbd8e01b63d2a
andrelmm91 <andre.lmm91@gmail.com>
2024-05-13 09:06:15 +0200

    Auth jwt init

--- BE/godependencies.m
+++ BE/godependencies.m
@@ -3,7 +3,10 @@ go get github.com/go-chi/chi/v5
 go get github.com/go-chi/chi/v5/middleware
 go get github.com/go-chi/cors
 
-//sql postgrtes
+// sql postgrtes
 go get github.com/jackc/pgx/v5
 go get github.com/jackc/pgconn
 go get github.com/jackc/pgx/v5/stdlib
+
+// jwt token authentication
+go get github.com/golang-jwt/jwt/v5
