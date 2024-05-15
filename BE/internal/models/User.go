794113fe68dee2706860a27b7694e7b6ccbcd58c
andrelmm91 <andre.lmm91@gmail.com>
2024-05-15 20:47:01 +0200

    fixing User struct time

--- BE/internal/models/User.go
+++ BE/internal/models/User.go
@@ -1,11 +1,13 @@
 package models
 
+import "time"
+
 type User struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	CreatedAt string `json:"-"`
-	UpdatedAt string `json:"-"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
