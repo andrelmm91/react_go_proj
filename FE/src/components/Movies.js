d41e116048f292faf3c8aa717275f70ffea909ea
andrelmm91 <110781476+andrelmm91@users.noreply.github.com>
2024-05-21 08:49:04 +0000

    textrea fe

--- FE/src/components/Movies.js
+++ FE/src/components/Movies.js
@@ -2,53 +2,56 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
+    useEffect( () => {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
 
-    const requestOptions = {
-      method: "GET",
-      headers: headers,
-    };
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
 
-    fetch(`http://localhost:8080/movies`, requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        setMovies(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+        fetch(`http://localhost:8080/movies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
-  return (
-    <div>
-      <h2>Movies</h2>
-      <hr />
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>Movie</th>
-            <th>Release Date</th>
-            <th>Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {movies.map((m) => (
-            <tr key={m.id}>
-              <td>
-                <Link to={`/movies/${m.id}`}>{m.title}</Link>
-              </td>
-              <td>{m.release_date}</td>
-              <td>{m.mpaa_rating}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+    }, []);
+
+    return(
+        <div>
+            <h2>Movies</h2>
+            <hr />
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((m) => (
+                        <tr key={m.id}>
+                            <td>
+                                <Link to={`/movies/${m.id}`}>
+                                    {m.title}
+                                </Link>
+                            </td>
+                            <td>{m.release_date}</td>
+                            <td>{m.mpaa_rating}</td>
+                        </tr>    
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
 
 export default Movies;
