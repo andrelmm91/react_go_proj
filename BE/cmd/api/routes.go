dc913df3c72f18fe8ec3cd1b0390392836ffb411
andrelmm91 <110781476+andrelmm91@users.noreply.github.com>
2024-05-28 09:48:34 +0000

    delete data in FE

--- BE/cmd/api/routes.go
+++ BE/cmd/api/routes.go
@@ -30,7 +30,7 @@ func (app *application) routes() http.Handler {
 		mux.Get("/movies", app.MovieCatalog)
 		mux.Get("/movies/{id}", app.MovieForEdit)
 		mux.Put("/movies/0", app.InsertMovie)
-		mux.Patch("/movies/0", app.UpdateMovie)
+		mux.Patch("/movies/{id}", app.UpdateMovie)
 	})
 
 	return mux
