0748d3d1264ab6af5ae64e845cc86bbea91a3416
andrelmm91 <110781476+andrelmm91@users.noreply.github.com>
2024-05-29 07:23:32 +0000

    delete date in BE, route, handler, and repo

--- BE/cmd/api/handlers.go
+++ BE/cmd/api/handlers.go
@@ -339,5 +339,25 @@ func (app *application) UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJSON(w, http.StatusAccepted, resp)
+}
 
+func (app *application) DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.DB.DeleteMovie(id)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	resp := JSONResponse{
+		Error: false,
+		Message: "movie deleted",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, resp)
 }
--- BE/internal/repo/repo.go
+++ BE/internal/repo/repo.go
@@ -7,14 +7,18 @@ import (
 
 type DatabaseRepo interface {
 	Connection() *sql.DB
-	AllMovies() ([]*models.Movie, error)
+	// get
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserById(id int) (*models.User, error)
-
+	AllMovies() ([]*models.Movie, error)
+	AllGenres() ([]*models.Genre, error)
 	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 	OneMovie(id int) (*models.Movie, error)
-	AllGenres() ([]*models.Genre, error)
+	// create
 	InsertMovie(movie models.Movie) (int, error)
-	UpdateMovieGenres(id int, genreIDs []int) error
+	// edit
 	UpdateMovie(movie models.Movie) error
+	UpdateMovieGenres(id int, genreIDs []int) error
+	// delete
+	DeleteMovie(id int) error 
 }
